a6f23838356c4ffa37015bb43b007b0b05884b5b
lex8978 <brunopicanco30@gmail.com>
2026-08-27 23:41:03 -0300

    Create questao7.php

---
+++ questao7.php
@@ -0,0 +1,5 @@
+<?php
+$valor = (float) readline("Digite o valor do produto : ");
+$quantidade = (float) readline("Digite a quantidade :");
+$soma = $quantidade * $valor;
+echo "O valor total da compra é : " .$soma;
