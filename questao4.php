27a24c542136be2dbe6f9dc9483d3253d3565a34
lex8978 <brunopicanco30@gmail.com>
2026-08-27 23:31:27 -0300

    Add basic arithmetic operations in questao4.php

---
+++ questao4.php
@@ -0,0 +1,8 @@
+<?php
+$nu1 =(int) readline("Digite o primeiro número : ");
+$nu2 =(int) readline("Digite o segundo número : ");
+$soma = $nu1 + $nu2;
+$mult = $nu1 * $nu2;
+$sub = $nu1 - $nu2;
+$div = $nu1 / $nu2;
+echo "seu resultado é : " .$soma . " e a multiplicação é : " .$mult . " e a subtração é : " .$sub . " e a divisão é : " .$div;
