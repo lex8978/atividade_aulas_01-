542fe69f897719e92223d6ca57815e5bdd347ad9
lex8978 <brunopicanco30@gmail.com>
2026-08-27 23:37:25 -0300

    Create questao6.php

---
+++ questao6.php
@@ -0,0 +1,6 @@
+<?php
+$nu1 =(int) readline("Digite o primeiro número : ");
+$nu2 =(int) readline("Digite o segundo número : ");
+$nu3 =(int) readline("Digite o terceiro número : ");
+$media = ($nu1 + $nu2 + $nu3)/ 3;
+echo "A média dos três números é: " . $media;
